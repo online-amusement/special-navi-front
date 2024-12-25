c2f51b7dd516e5202dd29f3f9bbeee5f9fb844c7
olp <t.miyai0001@gmail.com>
2024-12-25 09:25:55 +0900

    sidebase/nuxt-authの設定とsass-embedded,sass-loaderの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@sidebase/nuxt-auth'],
+  auth: {
+    globalAppMiddleware: true,
+    provider: {
+      type: 'local',
+      pages: {
+        login: '/',
+      },
+      endpoints: {
+        signIn: { path: "/login", method: "post"},
+        signOut: { path: "/logout", method: "get" },
+        getSession: { path: "/me", method: "post"},
+      },
+      token: {
+        // signInResponseTokenPointer: '/token', // レスポンスJSON内のtokenのキー
+        type: "Bearer",
+        headerName: 'Authorization', // Authorization
+        maxAgeInSeconds: 60 * 60 * 24 // バックエンドと同じか長くしないと不整合な状態になる（フロントが未ログイン、バックエンドがログイン中）
+      },
+    },
+  },
   runtimeConfig: {
     dev: process.env.API_URL !== 'production',
     public: {
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header-container">
+        <h2 class="header">美観地区</h2>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: center;
+    border-bottom: 1px solid #000;
+}
+</style>
